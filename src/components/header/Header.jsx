0c35a2eebf10c3f0edf5c01b15b1db62de73a6b0
BoomChawit <boomchawit.2203@gmail.com>
2022-03-15 14:16:43 +0900

    Edit Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <header>
       <div className="container header_container">
-        <h5>Hello, I'm</h5>
+        <h5>Hello, I am</h5>
         <h1>Boom Chawit</h1>
         <h5 className="text-light">Machine Learning Engineer</h5>
         <CTA />
